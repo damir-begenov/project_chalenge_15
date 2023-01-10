1e6049bc09a12ed0559b71cdc45d6feea1f538b3
Grisha <cocarecu@gmail.com>
2023-01-10 08:58:51 +0600

    Page for Schools are made by Maku and Tima

--- frontend/src/Components/Companies.js
+++ frontend/src/Components/Companies.js
@@ -1,43 +1,49 @@
 import { Component } from "react";
+import { Link } from 'react-router-dom'
+import { BrowserRouter as Router, Route} from "react-router-dom";
 import {Company} from "../CompanySch"
 import axios from 'axios';
 
 class Companies extends Component {
     state = {
-        schools: []
+        schools: [],
+        // links: []
     }
 
     componentDidMount() {
-        axios.get(`http://127.0.0.1:8081/alls/school`)
+        axios.get(`http://127.0.0.1:8012/alls/school`)
           .then(res => {
             const schools = res.data;
+            // const links = this.getLinks(schools)
             this.setState({ schools });
           })
       }
+
     render() {
         return(
-            <table class="table">
+        <table className="table">
             <thead >
                 <tr>
-                <th scope="col"><a class="sort">#</a></th>
-                <th scope="col"><a class="sort">BIN</a></th>
-                <th scope="col"><a class="sort">Company</a></th>
-                <th scope="col"><a class="sort">LABEL</a></th>
+                <th scope="col"><a className="sort">#</a></th>
+                <th scope="col"><a className="sort">BIN</a></th>
+                <th scope="col"><a className="sort">Company</a></th>
+                <th scope="col"><a className="sort">LABEL</a></th>
                 <th scope="col"></th>
                  </tr>
             </thead>
             <tbody>
-            {this.state.schools.map((person, index) => 
-                <tr className="row">
+                
+                {this.state.schools.map((person, index) => 
+            <tr className="row">
                 <th scope="row">{index+1}</th>
                 <td>{person.binid}</td>
-                <td className="FIO">{person.company}</td>
+                <td className="FIO"><Link className="rowInfo" to={`/schools/${person.binid}`}>{person.company}</Link></td>
                 <td>{person.label}</td>
             </tr>
+            
             )}
             </tbody>
-        </table>
-        )
+        </table>)
     }
 }
 
--- frontend/src/Components/Students.js
+++ frontend/src/Components/Students.js
@@ -2,6 +2,7 @@ import { Component, useEffect } from "react";
 import {StudentsList} from "../Students";
 import { useState } from "react";
 import axios from 'axios';
+import { Link } from 'react-router-dom'
  
 
 class AllStudents extends Component {
@@ -10,21 +11,30 @@ class AllStudents extends Component {
     }
 
     componentDidMount() {
-        axios.get(`http://127.0.0.1:8081/alls`)
+        axios.get(`http://127.0.0.1:8012/alls`)
           .then(res => {
             const persons = res.data;
             this.setState({ persons });
           })
       }
+      status(e) {
+        if (e.end_date == '' ) {
+            return <td className="finished">Studying</td>
+        } else {
+            return <td className="unfinished">Graduated</td>
+        }
+    }
     render() {
         return (
-        <table class="table">
-            <thead >
+        <table className="table marginTop">
+            <thead className="marginTop">
                 <tr>
                 <th scope="col"><a class="sort">#</a></th>
                 <th scope="col"><a class="sort">IIN</a></th>
                 <th scope="col"><a class="sort">FIO</a></th>
                 <th scope="col"><a class="sort">LABEL</a></th>
+                <th scope="col"><a class="sort">Status</a></th>
+                <th scope="col"><a class="sort">School</a></th>
                 <th scope="col"></th>
                  </tr>
             </thead>
@@ -32,9 +42,11 @@ class AllStudents extends Component {
             {this.state.persons.map((person, index) => 
                 <tr className="row">
                 <th scope="row">{index+1}</th>
-                <td className="IIN">{person.iinid}</td>
-                <td className="FIO">{person.fio}</td>
+                <td className="IIN"><Link className="rowInfo" to={`/students/${person.iinid}`}>{person.iinid}</Link></td>
+                <td className="FIO"><Link className="rowInfo" to={`/students/${person.iinid}`}>{person.fio}</Link></td>
                 <td>{person.label}</td>
+                {this.status(person.rel_finals[0])}
+                <td><Link className="rowInfo" to={`/schools/${person.rel_finals[0].node_c.binid}`}>{person.rel_finals[0].node_c.company}</Link></td>
             </tr>
             )}
             </tbody>
